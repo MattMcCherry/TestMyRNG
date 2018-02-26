2c9fdd291e97e09cb02ce6f82b5b99beb4b1204c
Matt McCherry <matt.mccherry@gmail.com>
2018-02-26 22:52:24 +0000

    Added item IDs to each item.
    These will be used to find item data from RS API for prices / item icons.

--- enemies.js
+++ enemies.js
@@ -6,56 +6,67 @@ const enemies = [
     drops: [
             {
                 name: 'Big bones', 
+                id: 532,
                 dropAmount: [1, 1], 
                 rarity: 100
             },
             {
-                name: 'Rune 2h sword', 
+                name: 'Rune 2h sword',
+                id: 1319,
                 dropAmount: [1, 1], 
                 rarity: 2/21
             },
             {
                 name: 'Rune pickaxe', 
+                id: 1275,
                 dropAmount: [1, 1], 
                 rarity: 2/21
             },
             {
                 name: 'Rune longsword',
+                id: 1303,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             },
             {
                 name: 'Rune platebody',
+                id: 1127,
                 dropAmount: [1, 1],
                 rarity:  2/21,
             },
             {
                 name: 'Bandos chestplate',
+                id: 11832,
                 dropAmount: [1, 1],
                 rarity: 1/384,
             },
             {
                 name: 'Bandos tassets',
+                id: 11834,
                 dropAmount: [1, 1],
                 rarity: 1/384,
             },
             {
                 name: 'Bandos boots',
+                id: 11836,
                 dropAmount: [1, 1],
                 rarity: 1/384,
             },
             {
                 name: 'Nature rune',
+                id: 561,
                 dropAmount: [40, 60],
                 rarity: 1/25,
             }, 
             {
                 name: 'Adamantite ore',
+                id: 449,
                 dropAmount: [15, 20],
                 rarity: 2/21,
             }, 
             {
                 name: 'Coal',
+                id: 453,
                 dropAmount: [115, 120],
                 rarity: 1/10,
             }, 
@@ -66,21 +77,25 @@ const enemies = [
             }, 
             {
                 name: 'Snapdragon seed',
+                id: 5300,
                 dropAmount: [1, 1],
                 rarity: 1/25,
             }, 
             {
                 name: 'Magic log',
+                id: 1513,
                 dropAmount: [15, 20],
                 rarity: 1/25,
             }, 
             {
                 name: 'Super restore(4)',
+                id: 3024,
                 dropAmount: [3, 3],
                 rarity: 1/25,
             }, 
             {
                 name: 'Grimy snapdragon',
+                id: 3051,
                 dropAmount: [3, 3],
                 rarity: 1/25,
             }, 
@@ -91,16 +106,19 @@ const enemies = [
             }, 
             {
                 name: 'Godsword shard 1',
+                id: 11818,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 2',
+                id: 11820,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 3',
+                id: 11822,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             },
@@ -111,6 +129,7 @@ const enemies = [
             },
             {
                 name: 'Bandos hilt',
+                id: 11812,
                 dropAmount: [1, 1],
                 rarity: 1/508,
             },
@@ -133,66 +152,79 @@ const enemies = [
     drops: [
             {
                 name: 'Big bones', 
+                id: 532,
                 dropAmount: [1, 1], 
                 rarity: 100
             },
             {
                 name: 'Feather', 
+                id: 314,
                 dropAmount: [1, 15], 
                 rarity: 100
             },
             {
                 name: 'Rune sword', 
+                id: 1289,
                 dropAmount: [1, 1], 
                 rarity: 1/406
             },
             {
                 name: 'Rune crossbow', 
+                id: 9185,
                 dropAmount: [1, 1], 
                 rarity: 2/21
             },
             {
                 name: `Black d'hide`,
+                id: 2503,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             },
             {
                 name: 'Armadyl helmet',
+                id: 11826,
                 dropAmount: [1, 1],
                 rarity:  1/384,
             },
             {
                 name: 'Armadyl chestplate',
+                id: 11828,
                 dropAmount: [1, 1],
                 rarity: 1/384,
             },
             {
                 name: 'Armadyl chainskirt',
+                id: 11830,
                 dropAmount: [1, 1],
                 rarity: 1/384,
             },
             {
                 name: 'Bandos boots',
+                id: 11836,
                 dropAmount: [1, 1],
                 rarity: 1/384,
             },
             {
                 name: 'Runite bolts',
+                id: 9144,
                 dropAmount: [18, 25],
                 rarity: 2/21,
             }, 
             {
                 name: 'Mind rune',
+                id: 558,
                 dropAmount: [500, 601],
                 rarity: 1/64, 
             }, 
             {
                 name: 'Rune arrow',
+                id: 892,
                 dropAmount: [100, 105],
                 rarity: 1/64, 
             }, 
             {
                 name: 'Dragonstone bolts(e)',
+                id: 9244,
                 dropAmount: [5, 15],
                 rarity: 1/64,
             }, 
@@ -208,31 +240,37 @@ const enemies = [
             }, 
             {
                 name: 'Dwarf weed',
+                id: 267,
                 dropAmount: [5, 22],
                 rarity: 2/21,
             }, 
             {
                 name: 'Dwarf weed seed',
+                id: 5303,
                 dropAmount: [3, 3],
                 rarity: 2/21,
             }, 
             {
                 name: 'Ranging potion(3)',
+                id: 169,
                 dropAmount: [3, 3],
-                rarity: 1/64, //uncommon
+                rarity: 1/64,
             }, 
             {
                 name: 'Super defence(3)',
+                id: 163,
                 dropAmount: [3, 3],
-                rarity: 1/64, //uncommon
+                rarity: 1/64,
             }, 
             {
                 name: 'Crystal key',
+                id: 989,
                 dropAmount: [3, 3],
-                rarity: 1/64, //uncommon
+                rarity: 1/64,
             }, 
             {
                 name: 'Yew seed',
+                id: 5315,
                 dropAmount: [1, 1],
                 rarity: 1/128, 
             }, 
@@ -243,16 +281,19 @@ const enemies = [
             }, 
             {
                 name: 'Godsword shard 1',
+                id: 11818,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 2',
+                id: 11820,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 3',
+                id: 11822,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             },
@@ -263,6 +304,7 @@ const enemies = [
             },
             {
                 name: 'Armadyl hilt',
+                id: 11810,
                 dropAmount: [1, 1],
                 rarity: 1/508,
             },
@@ -289,72 +331,86 @@ const enemies = [
                 rarity: 100
             },
             {
-                name: 'Adamant arrow(p++)', 
+                name: 'Adamant arrow(p++)',
+                id: 5626,
                 dropAmount: [295, 300], 
                 rarity: 2/21
             },
             {
-                name: 'Rune scimitar', 
+                name: 'Rune scimitar',
+                id: 1333,
                 dropAmount: [1, 1], 
                 rarity: 1/64
             },
             {
                 name: `Dragon dagger(p++)`,
+                id: 5698,
                 dropAmount: [1, 1],
                 rarity: 1/64,
             },
             {
                 name: 'Steam battlestaff',
+                id: 11787,
                 dropAmount: [1, 1],
                 rarity:  1/128,
             },
             {
                 name: 'Zamorakian spear',
+                id: 11824,
                 dropAmount: [1, 1],
                 rarity: 1/128,
             },
             {
                 name: 'Rune sword',
+                id: 1289,
                 dropAmount: [1, 1],
                 rarity: 1/406,
             },
             {
                 name: 'Staff of the dead',
+                id: 11791,
                 dropAmount: [1, 1],
                 rarity: 1/508,
             },
             {
                 name: 'Rune platelegs',
+                id: 1079,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             }, 
             {
                 name: 'Adamant platebody',
+                id: 1123,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             }, 
             {
                 name: 'Super attack(3)',
+                id: 145,
                 dropAmount: [3, 3],
                 rarity: 2/21,
             }, 
             {
                 name: 'Super strength(3)',
+                id: 157,
                 dropAmount: [3, 3],
                 rarity: 2/21,
             }, 
             {
                 name: 'Super restore(3)',
+                id: 3026,
                 dropAmount: [3, 3],
                 rarity: 2/21,
             }, 
             {
                 name: 'Zamorak brew(3)',
+                id: 189,
                 dropAmount: [3, 3],
                 rarity: 2/21,
             }, 
             {
                 name: 'Death rune',
+                id: 560,
                 dropAmount: [120, 125],
                 rarity: 2/21,
             }, 
@@ -390,16 +446,19 @@ const enemies = [
             }, 
             {
                 name: 'Godsword shard 1',
+                id: 11818,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 2',
+                id: 11820,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 3',
+                id: 11822,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             },
@@ -431,87 +490,104 @@ const enemies = [
     rolls: 2,
     drops: [
             {
-                name: 'Bones', 
+                name: 'Bones',
+                id: 526,
                 dropAmount: [1, 1], 
                 rarity: 100
             },
             {
-                name: 'Saradomin sword', 
+                name: 'Saradomin sword',
+                id: 11838,
                 dropAmount: [295, 300], 
                 rarity: 1/127
             },
             {
                 name: 'Armadyl crossbow', 
+                id: 11785,
                 dropAmount: [1, 1], 
                 rarity: 1/508
             },
             {
                 name: `Rune sword`,
+                id: 1289,
                 dropAmount: [1, 1],
                 rarity: 1/406,
             },
             {
                 name: 'Rune dart',
+                id: 811,
                 dropAmount: [35, 40],
                 rarity:  2/21,
             },
             {
                 name: 'Law rune',
+                id: 563,
                 dropAmount: [95, 105],
                 rarity: 2/21,
             },
             {
                 name: 'Rune plateskirt',
+                id: 1093,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             },
             {
                 name: 'Adamant platebody',
+                id: 1123,
                 dropAmount: [1, 1],
                 rarity: 1/64,
             },
             {
                 name: 'Rune kiteshield',
+                id: 1201,
                 dropAmount: [1, 1],
                 rarity: 1/64,
             }, 
             {
                 name: 'Grimy ranarr weed',
+                id: 207,
                 dropAmount: [5, 5],
                 rarity: 2/21,
             }, 
             {
                 name: 'Ranarr seed',
+                id: 5295,
                 dropAmount: [2, 2],
                 rarity: 2/21,
             }, 
             {
                 name: 'Prayer potion(4)',
+                id: 2434,
                 dropAmount: [3, 3],
                 rarity: 8/127,
             }, 
             {
                 name: 'Saradomin brew(3)',
+                id: 6687,
                 dropAmount: [3, 3],
                 rarity: 6/127,
             }, 
             {
                 name: 'Super restore(4)',
+                id: 3024,
                 dropAmount: [3, 3],
                 rarity: 6/127,
             },
             {
                 name: 'Super defence(4)',
+                id: 2442,
                 dropAmount: [3, 3],
                 rarity: 6/127,
             }, 
             {
                 name: 'Magic potion(3)',
+                id: 3042,
                 dropAmount: [3, 3],
                 rarity: 6/127,
             }, 
             {
                 name: 'Magic seed',
+                id: 5316,
                 dropAmount: [1, 1],
                 rarity: 1/128,
             }, 
@@ -522,6 +598,7 @@ const enemies = [
             }, 
             {
                 name: 'Diamond',
+                id: 1601,
                 dropAmount: [6, 6],
                 rarity: 1/64,
             }, 
@@ -532,6 +609,7 @@ const enemies = [
             }, 
             {
                 name: `Saradomin's light`,
+                id: 13256,
                 dropAmount: [1, 1],
                 rarity: 1/254,
             }, 
@@ -542,16 +620,19 @@ const enemies = [
             }, 
             {
                 name: 'Godsword shard 1',
+                id: 11818,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 2',
+                id: 11820,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 3',
+                id: 11822,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             },
@@ -574,101 +655,121 @@ const enemies = [
     drops: [
             {
                 name: 'Big bones', 
+                id: 532,
                 dropAmount: [1, 1], 
                 rarity: 100
             },
             {
                 name: 'Mole Claw', 
+                id: 7416,
                 dropAmount: [1, 1], 
                 rarity: 100
             },
             {
                 name: 'Mole skin', 
+                id: 7418,
                 dropAmount: [1, 3], 
                 rarity: 100
             },
             {
-                name: 'Air rune', 
+                name: 'Air rune',
+                id: 556,
                 dropAmount: [105, 105], 
                 rarity: 2/21
             },
             {
                 name: `Fire rune`,
+                id: 554,
                 dropAmount: [105, 105],
                 rarity: 2/21,
             },
             {
                 name: 'Blood rune',
+                id: 565,
                 dropAmount: [15, 15],
                 rarity:  2/21,
             },
             {
                 name: 'Death rune',
+                id: 560,
                 dropAmount: [7, 7],
                 rarity: 1/128,
             },
             {
                 name: 'Law rune',
+                id: 563,
                 dropAmount: [15, 15],
                 rarity: 1/64,
             },
             {
                 name: 'Iron arrow',
+                id: 884,
                 dropAmount: [690, 690],
                 rarity: 1/64,
             },
             {
                 name: 'Adamant longsword',
+                id: 1301,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             }, 
             {
                 name: 'Mithril axe',
+                id: 1355,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             }, 
             {
                 name: 'Mithril battleaxe',
+                id: 1369,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             }, 
             {
                 name: 'Mithril platebody',
+                id: 1121,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             }, 
             {
                 name: 'Rune med helm',
+                id: 1147,
                 dropAmount: [1, 1],
                 rarity: 1/64,
             }, 
             {
                 name: 'Amulet of strength',
+                id: 1725,
                 dropAmount: [1, 1],
                 rarity: 2/21,
             },
             {
                 name: 'Iron ore',
+                id: 440,
                 dropAmount: [100, 100],
                 rarity: 1/64,
             }, 
             {
                 name: 'Mithril bar',
+                id: 2359,
                 dropAmount: [1, 1],
                 rarity: 1/128,
             }, 
             {
                 name: 'Yew logs',
+                id: 1515,
                 dropAmount: [100, 100],
                 rarity: 5/64,
             }, 
             {
                 name: 'Shark',
+                id: 385,
                 dropAmount: [4, 4],
                 rarity: 1/64,
             }, 
             {
                 name: 'Oyster pearls',
+                id: 411,
                 dropAmount: [1, 1],
                 rarity: 1/128,
             }, 
@@ -689,16 +790,19 @@ const enemies = [
             }, 
             {
                 name: 'Godsword shard 1',
+                id: 11818,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 2',
+                id: 11820,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             }, 
             {
                 name: 'Godsword shard 3',
+                id: 11822,
                 dropAmount: [1, 1],
                 rarity: 1/256,
             },
