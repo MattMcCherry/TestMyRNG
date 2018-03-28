4d444f75c03a6a4123be40dfa563811bd7624b7b
Matt McCherry <matt.mccherry@gmail.com>
2018-03-28 10:19:35 +0100

    StopWatch Functional

--- appjQuery.js
+++ appjQuery.js
@@ -1,3 +1,50 @@
+// ******** STOPWATCH FUNCTIONS ******** \\
+
+var minutes = 0,
+    hours = 0,
+    strMins,
+    strHrs,
+    clearTime,
+    counting = false;
+
+function startStopWatch() {
+    if ( minutes === 60) {
+        minutes = 0; hours ++;
+    }
+    strHrs = ( hours < 10 ) ? ( `0${hours} : ` ) : ( `${hours} : ` );
+    strMins = ( minutes < 10 ) ? ( `0${minutes}` ) : ( minutes ); 
+    $('.counter').html(`${strHrs} ${strMins}`); 
+    minutes++; 
+    clearTime = setTimeout("startStopWatch()", 10 ); 
+    counting = true;
+}
+
+function stopStopWatch() {
+    if (counting) {
+        clearTimeout(clearTime);
+        counting = false;
+    }
+}
+
+function resetStopWatch() {
+    minutes = 0,
+    hours = 0,
+    strMins = `0${minutes}`,
+    strHrs = `0${minutes} : `,
+    clearTime,
+    counting = false;
+    $('.counter').html(`${strHrs} ${strMins}`); 
+}
+
+// ******** END OF STOPWATCH FUNCTIONS ******** \\
+
+function displayError(errorStr) {
+    var html = errorStr;
+    $(html).hide().addClass('error').appendTo('.error-container').fadeIn(1000).delay(1000).fadeOut(1000, function () {
+        $('.error').remove();
+    }); // fadeOut end
+}
+
 //setup boss selector
 $(enemies).each(function(i) {
     $('.boss').append($('<option/>')
@@ -6,17 +53,30 @@ $(enemies).each(function(i) {
 }); //ENEMIES EACH END
 
 $('.container .butt').on('click', 'button', function() {
+    
     if ($(this).text() === "START") {
         const bossVal = $('.boss').val();
         if (bossVal === 'empty') {
-            console.log('display error');
+            displayError('<p>Please select your boss before starting!</p>');
         } else { 
         console.log('do everything else');
+        startStopWatch();
+        $('.varbutt').text('STOP');
         const kph = $('.kph').val() || $(enemies)[bossVal].killsph;
         }
     }
+    
     if ($(this).text() === "STOP") {
-        console.log("stop");
+        if (counting) {
+            console.log("stop");
+            stopStopWatch();
+            $(this).text('RESET');
+        }
+    } else if ($(this).text() === "RESET") {
+        if (!counting) {
+            console.log("reset");
+            resetStopWatch();
+            $(this).text('STOP');
+        }
     }
 }); // START && STOP END ON CLICK
-
